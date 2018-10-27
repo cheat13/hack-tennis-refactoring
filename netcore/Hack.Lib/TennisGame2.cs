a6d0cd2be4549dd1529b0374f1049228cbed32fe
Kritsana <tragoolphrom@gmail.com>
2018-10-27 14:53:53 +0700

    remove unused method and rename

--- netcore/Hack.Lib/TennisGame2.cs
+++ netcore/Hack.Lib/TennisGame2.cs
@@ -15,7 +15,6 @@ namespace Hack.Lib
         public TennisGame2(string player1Name, string player2Name)
         {
             this.player1Name = player1Name;
-            p1point = 0;
             this.player2Name = player2Name;
         }
 
@@ -106,38 +105,12 @@ namespace Hack.Lib
             return score;
         }
 
-        public void SetP1Score(int number)
-        {
-            for (int i = 0; i < number; i++)
-            {
-                P1Score();
-            }
-        }
-
-        public void SetP2Score(int number)
-        {
-            for (var i = 0; i < number; i++)
-            {
-                P2Score();
-            }
-        }
-
-        private void P1Score()
-        {
-            p1point++;
-        }
-
-        private void P2Score()
-        {
-            p2point++;
-        }
-
         public void WonPoint(string player)
         {
             if (player == "player1")
-                P1Score();
+                p1point++;
             else
-                P2Score();
+                p2point++;
         }
 
     }
